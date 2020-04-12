2426d4c502732e498431c2fd4644b5adaf5f5d45
Florian Magalhaes <m.f.pro@outlook.com>
2020-04-12 20:57:59 +0200

    modification index et package

--- index.js
+++ index.js
@@ -2,20 +2,18 @@ const path        = require('path');
 const express     = require('express');
 const helmet      = require('helmet');
 const compression = require('compression');
-const vhost = require('vhost');
  
 // Déclaration du port HTTP à utiliser (généralement 80)
 const PORT = process.env.PORT || 8080;
  
 // Création d'un serveur web Express
 const app = express();
-exports.app = app;
+ 
 // Configuration du serveur Express
 app.disable('x-powered-by');
 app.use(helmet()); // Headers HTTP de protection standard
 app.use(compression()); // Headers HTTP pour la compression GZIP
-app.use(vhost('cv.florian-magalhaes.fr', require('/cv').app))
-// app.use(vhost('sync.mysite.com', require('/path/to/sync').app))
+app.use(express.static(path.join(__dirname, 'www'))); // Middleware pour gérer l'affichage des ressources statiques du dossier /www/
  
 // Démarrage du serveur Express
 app.listen(PORT, () => console.log(`✅  Le serveur est lancé et écoute sur le port :${PORT}`));
